db07aada91f09ded951750255f5ae2d3ef15a701
tib500 <72920795+tib500@users.noreply.github.com>
2021-09-02 19:22:53 +0200

    creation tableau

--- app.py
+++ app.py
@@ -9,6 +9,24 @@ app = Flask(__name__)
 def index():
     return render_template('tableRoom.html')
 
+@app.route('/generate', methods=['POST', 'GET'])
+def generate():
+    table = int(request.form['table'])
+    colonne = int(request.form['colonne'])
+    rangee = int(request.form['rangee'])
+    # roomName = int(request.form['roomName'])
+    Organisationclasse = [[ "" for i in range (rangee)] for i in range (colonne)]
+    if table == 1:
+        for i in range (colonne + colonne-1) :
+            if i%2 != 0 :
+                Organisationclasse.insert(i, [None for i in range (rangee)])
+    if table == 2:
+        for i in range (colonne + colonne-1) :
+            if i%3 == 0 :
+                Organisationclasse.insert(i, [None for i in range (rangee)])
+    print (Organisationclasse)
+    return ('test')
+
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=3000, debug=True)
