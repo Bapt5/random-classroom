2a3b4b7fc2e2a1576c614dc1f1255a1235aab9ac
Bapt5 <drouillet.baptiste@gmail.com>
2021-09-03 20:04:56 +0200

    gestion extension

--- app.py
+++ app.py
@@ -1,38 +1,47 @@
 from flask import Flask, render_template, request, redirect, session, url_for
-from flask_session import Session
 from flask.json import jsonify
 import csv
 import pandas as pd
 
 app = Flask(__name__)
 
+def getExtension(file):
+    return file.filename.split('.')[-1]
 
 @app.route('/')
 def index():
     return render_template('tableRoom.html')
 
-@app.route('/generate', methods=['POST', 'GET'])
+
+@app.route('/generate', methods=['POST'])
 def generate():
     table = int(request.form['table'])
     colonne = int(request.form['colonne'])
     rangee = int(request.form['rangee'])
     # roomName = int(request.form['roomName'])
     Organisationclasse = [[ "" if (table == 1 and j%2 == 0) or (table == 2 and (j+1)%3 !=0 ) else None for j in range(colonne * table + (colonne-1))] for i in range(rangee)]
-    print (Organisationclasse)
+    session['currentRoom'] = Organisationclasse
     return redirect(url_for('enterStudent'))
+
 
 @app.route('/enterStudent', methods=['GET'])
 def enterStudent():
     return render_template('enterStudent.html')
 
+
 @app.route('/tableau', methods=['POST'])
 def tableau():
-    excel = pd.read_excel(request.files.get('resume'))
-    excel.to_csv('listeEleve.csv')
-    with open('listeEleve.csv', newline='') as csvfile:
-        reader = csv.DictReader(csvfile)
-        for row in reader:
-            print("row")
+    file = request.files['eleves']
+    ext = getExtension(file)
+    if ext == 'xls' or ext == 'xlsx':
+        excel = pd.read_excel(file)
+        excel.to_csv('listeEleve.csv')
+        with open('listeEleve.csv', newline='') as csvfile:
+            reader = csv.DictReader(csvfile)
+    elif ext == 'csv':
+        reader = csv.DictReader(file.read())
+    # for row in reader:
+    #     print("row")
     return ('test')
 
 
